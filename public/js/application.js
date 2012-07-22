0b305495d50d0642e067c8b6ed1f6ceb9a1464d4
Kah Hong Tay <taykahhong@Kahs-MacBook-Air-3.local>
2012-07-22 03:00:37 -0700

    Update Splash CSS and calendar layout

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   checkResize();
-  $('input[type=text]').focus();
+  $('input[type=text]').focus();  
 
  	// Browser resize
  	$(window).resize(function() {
@@ -14,5 +14,20 @@ function checkResize() {
   
   $('.wrapper').height(h);    
   if (w > 1200)               
-    $('.wrapper').width(w);      
+    $('.wrapper').width(w);  
+    
+  NUM_DAYS = 3;
+
+  // Restyle interface if more than 3 days
+  if (NUM_DAYS > 3) {
+    $('.time-wrapper').width('37px');
+    $('.calendar-wrapper').css({ 'padding-left': '20px', 'padding-right': '20px' });
+    $('.calendar-wrapper').width(NUM_DAYS * 301 + 40);  
+    
+    var c = $('.calendar-wrapper').width();
+    var w = $('.main-wrapper').width();
+
+    $('.wrapper').width(c + w + 90);
+    $('.main-wrapper').width(w);
+  }    
 }
