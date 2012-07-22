072283d6cc8497953f06d2648a181373b1b30c14
Hai Dang Tran <tranhaidangtn10609@gmail.com>
2012-07-21 23:30:40 -0700

    Dropdown for city

--- routes/index.js
+++ routes/index.js
@@ -21,28 +21,22 @@ exports.getIter = function(req, res) {
   var constants = require("../configs/constants");
   var request = require('request');
   var _ = require('underscore');
+  var fs = require('fs');
 
-  var fs = constants.foursquare;
-
+  var foursquare = constants.foursquare;
 
   var city = "Sunnyvale,CA";
-  var fsAPI = "https://api.foursquare.com/v2/venues/search?";
-  var token = fs.access_token;
+  var fsAPI = foursquare.venueAPI;
+  var token = foursquare.access_token;
   var url = fsAPI+"near="+city+"&oauth_token="+token;
-
-    // res.render('foobar', { 
-    //   title : "FOO",
-    //   url : url
-    //     });
 
   request(url, function(err, resp, body) {
     var body = JSON.parse(body);
     var first = body.response.groups[0].items[0].name;
-    // var first = JSON.stringify();
-    // var abc = _.keys(body);
     res.render('foobar', { 
           title : "FOO",
           url : first
         });
   });
 }
+
